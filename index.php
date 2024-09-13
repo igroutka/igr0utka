2aa1e7566e58b68cb1f19ad135d3c8d368699f93
igroutka <118044135+igroutka@users.noreply.github.com>
2024-09-13 19:08:52 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background: linear-gradient(to bottom, #33ccff, #ff99cc);
+}
+.container {
+  text-align: center;
+  padding-top: 100px;
+}
+a {
+  color: white;
+  text-decoration: none;
+  font-size: 24px;
+  margin: 10px;
+}
+</style>
+</head>
+<body>
+<div class="container">
+  <a href="https://discord.com">Discord</a>
+  <a href="https://www.youtube.com">YouTube</a>
+  <a href="https://store.steampowered.com">Steam</a>
+</div>
+</body>
+</html>
